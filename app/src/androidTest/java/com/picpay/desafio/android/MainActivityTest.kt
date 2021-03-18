7ba98408ec73581e45b5dc9db91e740944d02796
Carlos A Krueger <crlalexandrek@gmail.com>
2021-03-18 05:04:35 -0300

    Update the shouldDisplayListItem test;

--- app/src/androidTest/java/com/picpay/desafio/android/MainActivityTest.kt
+++ app/src/androidTest/java/com/picpay/desafio/android/MainActivityTest.kt
@@ -1,12 +1,13 @@
 package com.picpay.desafio.android
 
+import android.view.View
 import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.platform.app.InstrumentationRegistry
+import com.picpay.desafio.android.ui.MainActivity
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
@@ -45,7 +46,31 @@ class MainActivityTest {
         server.start(serverPort)
 
         launchActivity<MainActivity>().apply {
-            // TODO("validate if list displays items returned by server")
+            moveToState(Lifecycle.State.RESUMED)
+            RecyclerViewMatchers.checkRecyclerViewItem(R.id.recyclerView, 0, isDisplayed())
+        }
+
+        server.close()
+    }
+
+    @Test
+    fun shouldNotDisplayListItem() {
+        server.dispatcher = object : Dispatcher() {
+            override fun dispatch(request: RecordedRequest): MockResponse {
+                return when (request.path) {
+                    "/users" -> successResponseWithInvalidUser
+                    else -> errorResponse
+                }
+            }
+        }
+
+        server.start(serverPort)
+
+        launchActivity<MainActivity>().apply {
+            moveToState(Lifecycle.State.RESUMED)
+            onView(withId(R.id.recyclerView)).check { view, _ ->
+                assert(view.visibility == View.GONE)
+            }
         }
 
         server.close()
@@ -57,6 +82,15 @@ class MainActivityTest {
         private val successResponse by lazy {
             val body =
                 "[{\"id\":1001,\"name\":\"Eduardo Santos\",\"img\":\"https://randomuser.me/api/portraits/men/9.jpg\",\"username\":\"@eduardo.santos\"}]"
+
+            MockResponse()
+                .setResponseCode(200)
+                .setBody(body)
+        }
+
+        private val successResponseWithInvalidUser by lazy {
+            val body =
+                "[{\"id\":null,\"name\":null,\"img\":null,\"username\":null}]"
 
             MockResponse()
                 .setResponseCode(200)
